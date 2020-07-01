1a4d61870dedeffa9a970e05bcc19ad16d6d9d76
Christofer Fellicious <christofer.fellicious@uni-passau.de>
2020-07-01 10:54:02 +0200

    Initial commit

--- pypads_onto/bindings/hooks.py
+++ pypads_onto/bindings/hooks.py
@@ -1,1 +1,4 @@
-DEFAULT_PADRE_HOOK_MAPPING = {}
+
+DEFAULT_PADRE_HOOK_MAPPING = {
+    "URILogger": {"on": ["pypads_fit", "pypads_predict", "pypads_transform"]}
+}
---
+++ pypads_onto/injections/loggers/uri_tracking.py
@@ -0,0 +1,48 @@
+import sys
+import os
+from pypads.app.injections.base_logger import LoggingFunction
+from pypads.injections.analysis.call_tracker import LoggingEnv
+from pypads.utils.logging_util import WriteFormats, try_write_artifact
+from pypads.importext.mappings import LibSelector
+
+
+class URILogger(LoggingFunction):
+    @staticmethod
+    def _needed_packages():
+        pass
+
+    def __pre__(self, ctx, *args, _pypads_write_format=WriteFormats.pickle, _pypads_env, _args, _kwargs, **kwargs):
+        for i in range(len(args)):
+            arg = args[i]
+            name = os.path.join(_pypads_env.call.to_folder(),
+                                "args",
+                                str(i) + "_" + str(id(_pypads_env.callback)))
+            try_write_artifact(name, arg, _pypads_write_format)
+
+        for (k, v) in kwargs.items():
+            name = os.path.join(_pypads_env.call.to_folder(),
+                                "kwargs",
+                                str(k) + "_" + str(id(_pypads_env.callback)))
+            try_write_artifact(name, v, _pypads_write_format)
+
+    def __post__(self, ctx, *args, _pypads_env:LoggingEnv,
+                 _pypads_pre_return, _pypads_result, _args, _kwargs, **kwargs):
+        """
+
+        :param ctx:
+        :param args:
+        :param _pypads_env:
+        :param _pypads_pre_return:
+        :param _pypads_result:
+        :param _args:
+        :param _kwargs:
+        :param kwargs:
+        :return:
+        """
+
+        from pypads.app.pypads import get_current_pads
+        pads = get_current_pads()
+
+
+
+
